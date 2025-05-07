8494c55966cf683fe6ee20512f12918538785d68
SantiagoPanozzo <santiagopanozzoa@outlook.com>
2025-05-06 22:12:52 -0300

    Added profile carousel and more serious UI
    - Added profile carousel
    - Changed the image carousel for a sponsor logos carousel at the bottom
    - Made text improvements to make it more serious and real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,41 @@
 import Image from "next/image";
 import HorizontalImageScroller from './components/horizontalImageScroller';
-import ProfileCard from './components/profileCard';
+import ProfileCarousel from './components/profileCarousel';
 
 export default function Home() {
   return (
     <div className="min-h-screen w-screen grid grid-rows-[auto_1fr_auto] justify-items-center font-[family-name:var(--font-geist-sans)] px-4 sm:px-0">
       <main className="flex flex-col items-center w-full max-w-3xl gap-10 pt-10 text-center">
-        <div className="w-full opacity-50">
-          <HorizontalImageScroller />
-        </div>
 
         <Image
-          src="/logo_ucu.svg"
+          src="/ucx.png"
           alt="UCU logo"
-          width={180}
-          height={38}
+          width={300}
+          height={150}
           priority
         />
 
-        <p className="text-lg font-[family-name:var(--font-geist-mono)]">
-          UCU Evento
-        </p>
+        <h1 className="text-6xl font-[family-name:var(--font-geist-mono)]">
+          UCX
+        </h1>
 
         <div className="lalista w-full">
           <ol className="list-inside list-decimal text-sm text-left sm:text-left font-[family-name:var(--font-geist-mono)] space-y-2 px-4 sm:px-0">
-            <li>Una cosa increíble</li>
-            <li>Tremendo y eso, viene el fantasma de Steve Jobs</li>
-            <li>En realidad no, es lo que es</li>
+            <h3>Un evento en el que se congregan grandes eminencias del marketing de servicios.</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+            </p>
           </ol>
         </div>
 
         <h2 className="text-4xl font-bold">¿Quién viene?</h2>
 
         <div className="w-full flex justify-center">
-          <ProfileCard
-            name="Ildefonso"
-            shortDescription="Marketinero"
-            description="El más grande profesional del marketing, una verdadera máquina de marketinear cosas marketineables. La leyenda del marketing realmente. La cabra."
-            image="https://i.pravatar.cc/300"
-          />
+          <ProfileCarousel />
+        </div>
+
+        <h2 className="text-4xl font-bold">Patrocinan:</h2>
+        <div className="w-full opacity-50">
+          <HorizontalImageScroller />
         </div>
       </main>
     </div>
